d4baf2afe9583d56b3c8f8741a223b46dab5bd36
arthurarrudaa <arthur.arruda.dev@gmail.com>
2026-09-12 23:03:48 -0300

    feat: adicionando perguntas ao FAQ

--- Eco-Mobitily/src/pages/Faq.tsx
+++ Eco-Mobitily/src/pages/Faq.tsx
@@ -16,6 +16,25 @@ function Faq() {
             soluções de transporte urbano inteligente e sustentável.
           </p>
         </div>
+
+        <div className="faq-item">
+          <h3>Quais são as missões?</h3>
+          <ol className="lista-missões">
+            <li>Ônibus Sustentável</li>
+            <li>Metrô Inteligente</li>
+            <li>Pedalada Verde</li>
+            <li>Carona Compartilhada</li>
+            <li>Caminhada Saudável</li>
+          </ol>
+        </div>
+
+        <div className="faq-item">
+          <h3>Como faço para trocar meus pontos?</h3>
+          <p>
+            Para trocar seus pontos, você pode acessar nosso sistema, lá você
+            terá acesso aos seus pontos, e poderá trocá-los facilmente.
+          </p>
+        </div>
       </section>
     </>
   )
