30d111935df34d2dba311389215081b13a958c21
arthurarrudaa <arthur.arruda.dev@gmail.com>
2026-09-12 22:43:32 -0300

    feat: monta estrutura da página inicial home

---
+++ Eco-Mobitily/src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import { Cabecalho } from '../components/Cabecalho'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <Cabecalho titulo="Início" />
+
+      <main>
+        <section>
+          <div>
+            <h1 className="index-cabecalho">
+              Contextualização Inicial do Projeto
+            </h1>
+
+            <p>
+              Nós escolhemos o segundo desafio proposto dentro dos 3 possíveis
+              no Challenge Soul Up.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <h2>Pontos Ecológicos</h2>
+
+          <p>
+            Os pontos ecológicos fazem parte da proposta desenvolvida pelo
+            nosso projeto.
+          </p>
+        </section>
+
+        <section>
+          <h2>Considerações Finais</h2>
+
+          <p>
+            Analisando o que o desafio propõe, acreditamos que somos capazes
+            de contribuir para melhorar esse cenário.
+          </p>
+        </section>
+      </main>
+
+      <footer>
+        <p>
+          &copy; Todos os direitos reservados - ECO Mobitily SoulUp - 2026
+        </p>
+      </footer>
+    </>
+  )
+}
+
+export default Home
+
