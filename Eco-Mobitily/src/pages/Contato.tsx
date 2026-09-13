83a18ec46470a0d7a27c45535f6db2eb570b3ed3
dhiegoeblmonte <dhiegobelmonte72@gmail.com>
2026-09-13 00:32:34 -0300

    feat: adiciona formulário controlado na página de contato

--- Eco-Mobitily/src/pages/Contato.tsx
+++ Eco-Mobitily/src/pages/Contato.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import { Cabecalho } from '../components/Cabecalho'
 import whatsappImg from '../assets/img/whatsapp.jpg'
 import './Contato.css'
 
 function Contato() {
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [mensagem, setMensagem] = useState('')
+
+  
   return (
     <>
       <Cabecalho titulo="Como gostaria de nos contatar?" />
@@ -25,6 +31,36 @@ function Contato() {
             <b>E-mail:</b> giovanedc2007@gmail.com
           </p>
         </div>
+        <form id="meuFormulario">
+  <h2>Envie uma mensagem</h2>
+
+  <label htmlFor="nome">Nome</label>
+  <input
+    id="nome"
+    type="text"
+    value={nome}
+    onChange={(evento) => setNome(evento.target.value)}
+  />
+
+  <label htmlFor="email">E-mail</label>
+  <input
+    id="email"
+    type="email"
+    value={email}
+    onChange={(evento) => setEmail(evento.target.value)}
+  />
+
+  <label htmlFor="mensagem">Mensagem</label>
+  <textarea
+    id="mensagem"
+    value={mensagem}
+    onChange={(evento) => setMensagem(evento.target.value)}
+  />
+
+  <button type="submit">
+    Enviar
+  </button>
+</form>
       </main>
     </>
   )
